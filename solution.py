46e0aef28b3b715b5a8176e58a0d7490d815d925
Nikolay Anisimov <82103870+anisimov99@users.noreply.github.com>
2023-04-07 15:46:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,18 @@
 import pandas as pd
+import pandas as pd
 import numpy as np
 
+from scipy.stats import gamma
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 784892881
 
-def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+def solution(p: float, x: np.array) -> tuple:
+
+    alpha = 1 - p
+    sum_r2 = np.sum(x*x)
+    N = len(x)
+
+    left_q = gamma.ppf(alpha/2, a=N, scale=2/N)
+    right_q = gamma.ppf(1-alpha/2, a=N, scale=2/N)
+
+    return np.sqrt(sum_r2/15/N/right_q), np.sqrt(sum_r2/15/N/left_q)
